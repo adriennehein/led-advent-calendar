6668407d812293b4e6b7f7675e5339d97aed239f
Adrienne Hein <adrienne.hein@gmail.com>
2021-09-26 12:10:31 -0700

    Added debounce logic to help with bugginess, better but not fixed-- wiring issue for sure. Changed light up from colorWipe to updating individual pixels, as it was creating a delay with more button presses.

--- led_advent_calendar.ino
+++ led_advent_calendar.ino
@@ -1,3 +1,4 @@
+
 #include <Adafruit_NeoPixel.h>
 
 #ifdef __AVR__
@@ -6,7 +7,20 @@
 
 const int BUTTON_PIN = 2;  // Connect the Button to pin 2 or change here
 const int LED_PIN    = 6; // Connect the LED to pin 6 or change here
+const int DEBOUNCE_DELAY = 50;   // the debounce time; increase if the output flickers
 
+// Variables will change:
+int lastSteadyState = LOW;       // the previous steady state from the input pin
+int lastFlickerableState = LOW;  // the previous flickerable state from the input pin
+int currentState;                // the current reading from the input pin
+
+//boolean oldState = HIGH;
+int PIXEL_INDEX = 0;
+int BTN_COUNT = 0;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -17,45 +31,68 @@ const int LED_PIN    = 6; // Connect the LED to pin 6 or change here
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(50, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-boolean oldState = HIGH;
-int PIXEL_INDEX = 0;
-int BTN_COUNT = 0;
+
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(BUTTON_PIN, INPUT_PULLUP); // set arduino pin to input pull-up mode
 
-  strip.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show(); // Turn OFF all pixels ASAP
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
 
 }
 
 void loop() {
 
   // Get current button state.
-  boolean newState = digitalRead(BUTTON_PIN);
-    // Check if state changed from high to low (button press).
-  if((newState == LOW) && (oldState == HIGH)) {
-    // Short delay to debounce button.
-    delay(20);
-    // Check if button is still low after debounce.
-    newState = digitalRead(BUTTON_PIN);
-    if(newState == LOW) {      // Yes, still low
+  currentState = digitalRead(BUTTON_PIN);
+
+    // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch/button changed, due to noise or pressing:
+  if (currentState != lastFlickerableState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+    // save the the last flickerable state
+    lastFlickerableState = currentState;
+  }
+
+    if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if(lastSteadyState == HIGH && currentState == LOW) {
+      Serial.println("The button is pressed");
         if (BTN_COUNT < 25) {
-            colorWipe(strip.Color( 127,   127,   0), 50); 
+//            colorWipe(strip.Color(127, 127, 0), 50);  
+
+            strip.setPixelColor(PIXEL_INDEX, strip.Color(127, 127, 0));
+            strip.show();
+            delay(50);
 
 //          pixels.setPixelColor(PIXEL_INDEX, pixels.Color( 255,   204,   102));
 //          pixels.show();  // Send the updated pixel colors to the hardware.
           PIXEL_INDEX = PIXEL_INDEX + 2; // increment two to light up every even indexed light
           BTN_COUNT = BTN_COUNT + 1; // incrememnt btn presses to determine when 25 is reached for XMAS;
+          Serial.println(BTN_COUNT);
         }
         if (BTN_COUNT == 25) {
-           colorWipe(strip.Color( 255,   0,   0), 50); 
+            rainbow(50);   
         }
-      }
+        
     }
- 
-  // Set the last-read button state to the old state.
-  oldState = newState;
+    else if(lastSteadyState == LOW && currentState == HIGH) {
+      Serial.println("The button is released");
+    }
+    // save the the last steady state
+    lastSteadyState = currentState;
+  }
+  
+   
 }
 
 
@@ -74,43 +111,43 @@ void colorWipe(uint32_t c, uint8_t wait) {
 // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
 // a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
 // between frames.
-//void theaterChase(uint32_t color, int wait) {
-//  for(int a=0; a<10; a++) {  // Repeat 10 times...
-//    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-//      strip.clear();         //   Set all pixels in RAM to 0 (off)
-//      // 'c' counts up from 'b' to end of strip in steps of 3...
-//      for(int c=b; c<strip.numPixels(); c += 3) {
-//        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-//      }
-//      strip.show(); // Update strip with new contents
-//      delay(wait);  // Pause for a moment
-//    }
-//  }
-//}
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-//void rainbow(int wait) {
-//  // Hue of first pixel runs 3 complete loops through the color wheel.
-//  // Color wheel has a range of 65536 but it's OK if we roll over, so
-//  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
-//  // means we'll make 3*65536/256 = 768 passes through this outer loop:
-//  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
-//    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-//      // Offset pixel hue by an amount to make one full revolution of the
-//      // color wheel (range of 65536) along the length of the strip
-//      // (strip.numPixels() steps):
-//      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-//      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-//      // optionally add saturation and value (brightness) (each 0 to 255).
-//      // Here we're using just the single-argument hue variant. The result
-//      // is passed through strip.gamma32() to provide 'truer' colors
-//      // before assigning to each pixel:
-//      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-//    }
-//    strip.show(); // Update strip with new contents
-//    delay(wait);  // Pause for a moment
-//  }
-//}
+void rainbow(int wait) {
+  // Hue of first pixel runs 3 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 3*65536/256 = 768 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
 
 // Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
 //void theaterChaseRainbow(int wait) {
