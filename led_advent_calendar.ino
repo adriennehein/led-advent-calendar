d14ab5b965279e8e8a61e95e43b1e24a82884d7b
Adrienne Hein <adrienne.hein@gmail.com>
2021-10-25 17:32:57 -0700

    Fixed indeces of pixels according to how physical calendar was built. Added pixel off feature on 26th button press. Moved button incrementing to non-buggy location.

--- led_advent_calendar.ino
+++ led_advent_calendar.ino
@@ -17,6 +17,7 @@ int currentState;                // the current reading from the input pin
 //boolean oldState = HIGH;
 int PIXEL_INDEX = 0;
 int BTN_COUNT = 0;
+int pixels[] = {0, 2, 4, 6, 8, 9, 10, 12, 14, 16, 18, 21, 23, 25, 26, 27, 29, 31, 33, 34, 36, 38, 40, 42, 44, 45, 46}; // due to LED layout, these are the pixels that need to lit up
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -68,21 +69,26 @@ void loop() {
     if(lastSteadyState == HIGH && currentState == LOW) {
       Serial.println("The button is pressed");
         if (BTN_COUNT < 25) {
-//            colorWipe(strip.Color(127, 127, 0), 50);  
-
-            strip.setPixelColor(PIXEL_INDEX, strip.Color(127, 127, 0));
+            strip.setPixelColor(pixels[PIXEL_INDEX], 10157824); //warm yellow
             strip.show();
             delay(50);
-
 //          pixels.setPixelColor(PIXEL_INDEX, pixels.Color( 255,   204,   102));
 //          pixels.show();  // Send the updated pixel colors to the hardware.
-          PIXEL_INDEX = PIXEL_INDEX + 2; // increment two to light up every even indexed light
-          BTN_COUNT = BTN_COUNT + 1; // incrememnt btn presses to determine when 25 is reached for XMAS;
+          PIXEL_INDEX = PIXEL_INDEX + 1; // increment index of pixel array
           Serial.println(BTN_COUNT);
         }
         if (BTN_COUNT == 25) {
-            rainbow(50);   
+            christmas(50);
         }
+        if (BTN_COUNT >= 26) {
+          // turn off pixels on 26th button press
+          strip.clear();
+          strip.show();
+          BTN_COUNT = 0;
+          PIXEL_INDEX = 0;
+        }
+        
+        BTN_COUNT = BTN_COUNT + 1; // incrememnt btn presses to determine when 25 is reached for XMAS
         
     }
     else if(lastSteadyState == LOW && currentState == HIGH) {
@@ -91,8 +97,7 @@ void loop() {
     // save the the last steady state
     lastSteadyState = currentState;
   }
-  
-   
+
 }
 
 
@@ -106,7 +111,30 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void christmas(int wait) {
+  // Hue of first pixel runs 3 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 3*65536/256 = 768 passes through this outer loop:
+  int arraySize = sizeof( pixels );
+  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
+    for(int i=0; i<27; i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(pixels[i], strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
 
 // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
 // a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
@@ -126,28 +154,28 @@ void theaterChase(uint32_t color, int wait) {
 }
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 3 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 3*65536/256 = 768 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-  }
-}
+// void rainbow(int wait) {
+//   // Hue of first pixel runs 3 complete loops through the color wheel.
+//   // Color wheel has a range of 65536 but it's OK if we roll over, so
+//   // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+//   // means we'll make 3*65536/256 = 768 passes through this outer loop:
+//   for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
+//     for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+//       // Offset pixel hue by an amount to make one full revolution of the
+//       // color wheel (range of 65536) along the length of the strip
+//       // (strip.numPixels() steps):
+//       int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+//       // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+//       // optionally add saturation and value (brightness) (each 0 to 255).
+//       // Here we're using just the single-argument hue variant. The result
+//       // is passed through strip.gamma32() to provide 'truer' colors
+//       // before assigning to each pixel:
+//       strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+//     }
+//     strip.show(); // Update strip with new contents
+//     delay(wait);  // Pause for a moment
+//   }
+// }
 
 // Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
 //void theaterChaseRainbow(int wait) {
